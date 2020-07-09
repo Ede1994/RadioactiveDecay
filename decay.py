b351b53e01a74c58ae101b14f5a8b56294d68d10
Eric Einspänner <60260090+Ede1994@users.noreply.github.com>
2020-07-09 15:45:28 +0200

    Update decay.py
    code resorted; add new nuclides; add half life timesteps

--- decay.py
+++ decay.py
@@ -1,24 +1,26 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-@author: eric
+Author: Eric Einspänner, Institute of Nuclear Medicine, UMG (Germany)
+
+This program is free software.
 """
 import math
 import time
 from datetime import datetime
-import numpy as np
 import matplotlib.pyplot as plt
 
 
 # nuclides dictionary with half life in s
 nuclides = {
-		"C-10": 19.29,
+        "O-15": 122.24,
+        "N-13": 597.9,
+		"Tc-99m": 21.624*10**3,
+        "I-131": 693*10**3,
 		"F-18": 6.586*10**3
 		}
 
-print(nuclides)
-for key in nuclides:
-	print(key)
+
 
 # definition decay constant factor
 def decay_const(t):
@@ -34,13 +36,28 @@ def decay_equation(a0,c,t):
 	return a
 
 
-# set and convert start time
+# set up nuclid (user input)
+nuclid_input = input('Nuclid (e.g. F-18): ')
+
+
+# compare user input with nuclides dict
+for key in nuclides:
+	if nuclid_input == key:
+		nuclid = key
+		half_life = nuclides.get(key)
+
+
+# set starting activity
+activity = float(input('Starting activity in Bq: '))
+
+
+# set (user input) and convert start time
 date = input('Enter the start time (d/m/Y H:M:S): ')
 d = datetime.strptime(date, "%d/%m/%Y %H:%M:%S")
 start_time = time.mktime(d.timetuple())
 
 
-# set and convert end time
+# set (user input) and convert end time
 date = input('Enter the end time (d/m/Y H:M:S): ')
 d = datetime.strptime(date, "%d/%m/%Y %H:%M:%S")
 end_time = time.mktime(d.timetuple())
@@ -48,28 +65,11 @@ end_time = time.mktime(d.timetuple())
 
 # calculate the elapsed time
 elapsed_time = end_time - start_time
-print('Elapsed Time: {} s'.format(elapsed_time))
-
-
-# set starting activity
-activity = float(input('Starting activity in Bq: '))
-
-# set up nuclid (user)
-nuclid_input = input('Nuclid (e.g. F-18): ')
-
-
-for key in nuclides:
-	if nuclid_input == key:
-		nuclid = key
-		half_life = nuclides.get(key)
-
-# nuclid and half life
-#nuclid = 'Carbon-10'
-#half_life = 19.29 # carbon-10
+# print('Elapsed Time: {} s'.format(elapsed_time))
 
 
 # print all relevant infos
-print('Radioactive Decay - Nuclid: {}, Half life: {} s, Activity: {} Bq'.format(nuclid, half_life, activity))
+print('Radioactive Decay - Nuclid: {}, Half life: {} s, Activity: {} Bq, Elapsed Time: {} s'.format(nuclid, half_life, activity, elapsed_time))
 
 
 # specific activity
@@ -95,20 +95,26 @@ while t < (half_life * 4):
 	t += dt
 
 
+# list with 1st, 2nd and 3rd half life for timesteps in plot
+half_life_timesteps = [half_life, half_life*2, half_life*3]
+
+
 # plot graph of activity depends on time
 fig = plt.figure()
 ax = fig.add_subplot(111)
-line, = ax.plot(tlist, alist, color='#ee8d18', lw=3)
+line, = ax.plot(tlist, alist, color='#ee8d18', lw=2)
+
+# plot half life timesteps
+for xcoord in half_life_timesteps:
+    plt.axvline(x = xcoord, color='red', linestyle='--', linewidth='0.33')
+
 # plot single point (elapsed time)
 ax.plot([elapsed_time], [activity_elapsed_time], 'o')
 ax.annotate('You are here!',
             xy=(elapsed_time, activity_elapsed_time),
-            #xytext=(0.1, 0.1),    # fraction, fraction
-            #textcoords='figure fraction',
-            #arrowprops=dict(facecolor='black', shrink=0.05),
-            #horizontalalignment='left',
-            #verticalalignment='bottom',
             )
+
+# set up labeling
 plt.title('Nuclid: {}, Half life: {} s, Activity: {} Bq'.format(nuclid, half_life, activity))
 plt.xlabel('Time (s)')
 plt.ylabel('Activity (Bq)')
