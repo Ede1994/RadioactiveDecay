c7f3cb2bb2a630b616847f96bd91e4ffe9ee0591
Ede1994 <eric.einspa@hotmail.de>
2020-07-10 14:40:37 +0200

    Add outsourcing.py: contains deleted code

---
+++ outsourcing.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+# compare user input with nuclides dict
+for key in nuclides:
+	if nuclid_input == key:
+		nuclid = key
+		half_life = nuclides.get(key)
+"""
+
